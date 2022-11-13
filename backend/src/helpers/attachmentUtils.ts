c91d8e8749d514b9dbd54fa49b0de8629e53150d
Oluwaseun Olorunsola <seun@oluwaseun.com.ng>
2022-11-13 18:30:36 +0100

    Completed Business Logic

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -1,6 +1,72 @@
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
+// import * as AWSXRay from 'aws-xray-sdk'
+import { createLogger } from '../utils/logger'
 
-const XAWS = AWSXRay.captureAWS(AWS)
+// const XAWS = AWSXRay.captureAWS(AWS)
 
-// TODO: Implement the fileStogare logic
+const logger = createLogger('AttachmentUtil')
+
+// The file Storage logic
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
+
+export class AttachmentUtil {
+  constructor(
+    private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
+    private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION
+  ) {}
+
+  /**
+   * Create Attachment Url
+   * in format: https://s3-bucket-name.s3.eu-west-2.amazonaws.com/image.png
+   * @param todoId
+   * @returns {string} Attachment URL
+   */
+  createAttachmentUrl(todoId: string): string {
+    return `https://${this.bucketName}.s3.amazonaws.com/${todoId}`
+  }
+
+  /**
+   * It returns a pre-signed URL that can be used to upload an attachment file for a TODO item.
+   *
+   * @param todoId Todo ID
+   * @returns {string} pre-signed URL
+   */
+  getUploadUrl(todoId: string): string {
+    return s3.getSignedUrl('putObject', {
+      Bucket: this.bucketName,
+      Key: todoId,
+      Expires: parseInt(this.urlExpiration, 10)
+    })
+  }
+
+  async getTodoAttachment(todoId: string): Promise<boolean> {
+    try {
+      const response = await s3
+        .getObject({
+          Bucket: this.bucketName,
+          Key: todoId
+        })
+        .promise()
+
+      if (response && response.Body) {
+        return !!response.Body
+      }
+    } catch (error) {
+      logger.error('[getTodoAttachment] error fetching s3 object: ', error)
+    }
+    return false
+  }
+
+  async deleteTodoAttachment(todoId: string): Promise<boolean> {
+    const response = await s3
+      .deleteObject({
+        Bucket: this.bucketName,
+        Key: todoId
+      })
+      .promise()
+
+    return response && response.DeleteMarker
+  }
+}
--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -1,10 +1,122 @@
 import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
-import * as createError from 'http-errors'
+import { AttachmentUtil } from './attachmentUtils'
+import { QxtraQueryParam } from '../models/ExtraQueryParam'
 
-// TODO: Implement businessLogic
+// BusinessLogic for todo app
+
+const todosAccess = new TodosAccess()
+const attachmentUtil = new AttachmentUtil()
+
+const logger = createLogger('BusinessLogic')
+
+async function checkUserCanAccessTodo(userId: string, todoId: string) {
+  if (!userId) {
+    // user not found
+    logger.info('[checkUserCanAccessTodo] user is not found.', { userId })
+    throw new Error(`User (${userId}) not found`)
+  }
+
+  if (!todoId) {
+    // todoId not found
+    logger.info('[checkUserCanAccessTodo] todoId not found.', { todoId })
+    throw new Error(`Todo ${todoId} not found`)
+  }
+
+  const currentTodo = await todosAccess.getTodoById(userId, todoId)
+
+  if (currentTodo.userId !== userId) {
+    // user not permited to update other users todo
+    logger.info('[checkUserCanAccessTodo] user is not able to access todo.', {
+      userId,
+      currentTodo
+    })
+    throw new Error(
+      `User (${userId}) not authorised to access todo: ${currentTodo.todoId}`
+    )
+  }
+
+  return true
+}
+
+export async function getTodoById(
+  userId: string,
+  todoId: string
+): Promise<TodoItem> {
+  return todosAccess.getTodoById(userId, todoId)
+}
+
+export async function getTodosForUser(
+  userId: string,
+  extraQueryParam: QxtraQueryParam = {}
+) {
+  return todosAccess.getAllTodosByUser(userId, extraQueryParam)
+}
+
+export async function createTodo(
+  userId: string,
+  todoBody: CreateTodoRequest
+): Promise<TodoItem> {
+  const todoId = uuid.v4()
+
+  const newTodoObj: TodoItem = {
+    todoId,
+    userId,
+    name: todoBody.name,
+    dueDate: todoBody.dueDate,
+    createdAt: new Date().toISOString(),
+    done: false,
+    attachmentUrl: attachmentUtil.createAttachmentUrl(todoId)
+  }
+
+  return todosAccess.createTodo(newTodoObj)
+}
+
+export async function updateTodo(
+  userId: string,
+  todoId: string,
+  todoBody: UpdateTodoRequest
+): Promise<void> {
+  await checkUserCanAccessTodo(userId, todoId)
+
+  return todosAccess.updateTodo(userId, todoId, todoBody)
+}
+
+export async function deleteTodo(userId: string, todoId: string) {
+  await checkUserCanAccessTodo(userId, todoId)
+
+  const todoDeleted = await todosAccess.deleteTodo(userId, todoId)
+
+  logger.info('[BL > deleteTodo] todoDeleted: ', todoDeleted)
+
+  if (!todoDeleted) return false
+
+  const checkForTodoAttachment = await attachmentUtil.getTodoAttachment(todoId)
+
+  logger.info(
+    '[BL > deleteTodo] checkForTodoAttachment: ',
+    checkForTodoAttachment
+  )
+
+  if (checkForTodoAttachment) {
+    // when a todo item is deleted, remove the todo Attachment
+    const deletedAttachment = await attachmentUtil.deleteTodoAttachment(todoId)
+
+    logger.info('[BL > deleteTodo] deletedAttachment: ', deletedAttachment)
+  }
+
+  return true
+}
+
+export async function createAttachmentPresignedUrl(
+  userId: string,
+  todoId: string
+) {
+  await checkUserCanAccessTodo(userId, todoId)
+
+  return attachmentUtil.getUploadUrl(todoId)
+}
